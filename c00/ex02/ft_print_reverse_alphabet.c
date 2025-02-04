e1406960a5de6aa3bdb20b27e1d07ee0c733d04b
Tiago Monteiro <157115749+TiagoDev88@users.noreply.github.com>
2025-02-04 14:41:30 +0000

    Add files via upload

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfilipe- <tfilipe-@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 15:27:34 by tfilipe-          #+#    #+#             */
+/*   Updated: 2025/02/03 15:27:53 by tfilipe-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	x;
+
+	x = 'z';
+	while (x >= 'a')
+	{
+		write(1, &x, 1);
+		x--;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+}*/
---
+++ c00/ex06/ft_print_comb2.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfilipe- <tfilipe-@student.42porto.coim>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 15:37:23 by tfilipe-          #+#    #+#             */
+/*   Updated: 2025/02/03 15:37:46 by tfilipe-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_putchar(a / 10 + '0');
+			ft_putchar(a % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(b / 10 + '0');
+			ft_putchar(b % 10 + '0');
+			if (!(a == 98 && b == 99))
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_comb2();
+}*/
